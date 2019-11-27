01fd4f30c3c772dbe4ce3e3f03a28373a8b8ef1f
Ibrahim Gokalp <gokalpi@gmail.com>
2019-11-27 10:40:13 +0300

    Auth0 authentication feature added

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,8 @@
 export const apiEndpoint = 'https://ab3tp3fzdc.execute-api.eu-central-1.amazonaws.com/dev'
+
+export const authConfig = {
+    domain: 'dev-lsutvgwy.auth0.com',
+    clientId: 'oW9mJzFBgtYiNS6Tj6GXlXhrvYBXU1Zq',
+    callbackUrl: 'http://localhost:3000/callback'
+  }
+  
